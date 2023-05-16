0c8f910ecbff361de71ff1b6a109a146ee35c3d6
Terry <misterfreemann@gmail.com>
2023-05-16 16:49:03 -0400

    fixed style About Me page, added portfolio image, fixed footer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
 import React from "react"
 import "./About.css"
+import me from "../assets/images/me.png";
 function About (){
     return(
-        <div>
-        <h1 className="aboutMe">About Me</h1>
+        // <div className="homeAboutMe">
+        <div className="sectionAboutMe">
+            <div>
+        <h2 className="aboutMe">My name is Terry</h2>
+        <h1 className="aboutMe">And I'm a <span>Web Developer</span></h1>
         <p className="aboutMe">Hello. My name is Terry: a newby full stack web developer, formerly a Project Manager in construction, living in Toronto, Canada. 
                 I am passionate about building simple and organic websites in HTML5, CSS and JavaScript. 
                 I am always looking to expand my skills, and am up for new challenges. 
@@ -11,7 +15,13 @@ function About (){
                 Please feel free to browse through my portfolio.    
             </p>
             </div>
+            <div>
+            <img className="aboutMe" id="myPicture" src={me} alt="profilePicture"></img>
+            </div>
+            </div>
+            // </div>
     )
 }
 
 export default About;
+
